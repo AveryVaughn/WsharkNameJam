b0a8b74452e9b0ba5e3006aa84d404cadcfc13c4
Avery Vaughn <nokizaru_901@protonmail.com>
2021-09-11 00:08:44 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+For Use Under Duress.
+Cobbled T0gether and Messy only
+1. Download in new folder
+2. Download ip_range.txt file. place in same folder
+3. Run "runMe.py"
+4. copy the output of the script
+5. paste into Wireshark display filter
+6. record unique ips (make sure to exclude the LP ip)
+ProTip: run the capture without the filter to deduce the LP ip address
+7. Copy All ips line by line in the flag box
+8. get points
+
+My code may be messy, I come from a less elegant time
+tested and 0wnd
---
+++ runMe.py
@@ -0,0 +1,33 @@
+# cheap and dirty script to generate that endlessly long wireshark filter
+# make sure you get the basic concept of what the filters are looking for 
+# or this is easy mode
+# keep circulating the tapes
+def dumpOutString(s):
+    sUsed1 = f"{s[0]}".strip()
+    thisCounter = 1
+    thisUpperLimit = len(s) 
+    while thisCounter <= thisUpperLimit - 1 :
+        sUsed1 += s[thisCounter].strip() + " "
+        thisCounter += 1
+        
+    return sUsed1
+
+
+fileToUse = open("ip_ranges.txt", 'r')
+theseIps = fileToUse.readlines()
+firstipString = f"ip.addr == {theseIps[0]} "
+ipList = []
+ipList.insert(0, firstipString)
+counter = 1
+upperLimit = len(theseIps) 
+while counter < upperLimit:
+    toAdd = f"or ip.addr == {theseIps[counter]}"
+    ipList.append(toAdd)
+    counter += 1
+
+
+bigTime = dumpOutString(ipList)
+print(bigTime)
+
+
+
